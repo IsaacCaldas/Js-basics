45e027793df5d768fb1801983885dc2683cf97fa
Isaac Tavares Caldas Gonçalves <isaaccaldasgoncalves@gmail.com>
2022-06-26 00:09:10 -0300

    var

---
+++ 1.fundamentos/var_let_const.js
@@ -0,0 +1,12 @@
+var a = 3
+let b = 5
+
+var a = 30
+// can't redeclare a variables declared with let
+b = 40
+
+console.log(a, b)
+
+const c = 5
+// can't redeclare and add other value a const
+//c = 50 
