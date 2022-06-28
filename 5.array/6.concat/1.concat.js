d1f18ff125e5b5857d4f10f132e5182b000aa066
Isaac Tavares Caldas Gonçalves <isaaccaldasgoncalves@gmail.com>
2022-06-28 14:35:19 -0300

    Concat()

---
+++ 5.array/6.concat/1.concat.js
@@ -0,0 +1,7 @@
+const daughters = ['Mary', 'Eliza']
+const sons = ['Javier', 'Levi']
+
+const childrens = daughters.concat(sons)
+console.log(childrens, daughters, sons)
+
+console.log([].concat([1, 2], [3, 6], -1, [22, [0, -2, 3], 3, [10, 55, [23, [33, 32, 22], [111, 100]]]]))
