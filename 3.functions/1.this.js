62c6ae36fae06dd610becba846722705607e8bab
Isaac Tavares Caldas Gonçalves <isaaccaldasgoncalves@gmail.com>
2022-06-28 10:03:55 -0300

    Bind

---
+++ 3.functions/1.this.js
@@ -0,0 +1,14 @@
+const student = {
+  name: 'John Doe',
+  sayHello() {
+    console.log(`Hello ${this.name}!`) 
+  }
+}
+
+student.sayHello()
+const say = student.sayHello
+say()
+
+const sayBinded = student.sayHello.bind(student)
+sayBinded()
+
