056afb9a9ac5650cc6afb19a368590fdaf1d8a96
Isaac Tavares Caldas Gonçalves <isaaccaldasgoncalves@gmail.com>
2022-06-28 13:21:05 -0300

    Prototype: map

---
+++ 5.array/2.map/3.map_prototype.js
@@ -0,0 +1,21 @@
+Array.prototype.proto_map = function(callback) {
+  const new_array = []
+  for(let i = 0; i < this.length; i++) {
+    new_array.push(callback(this[i], i, this))
+  }
+  return new_array
+}
+
+const cart = [
+  '{ "name": "Pencil", "price": 0.49 }',
+  '{ "name": "Book", "price": 13.99 }',
+  '{ "name": "Notebook", "price": 5.00 }',
+  '{ "name": "Schoolbag", "price": 35.00 }',
+  '{ "name": "Ruler", "price": 2.00 }'
+]
+
+cart.proto_map(product => {
+  let product_json = JSON.parse(product)
+  let price = parseFloat(product_json.price).toFixed(2).replace('.', ',')
+  console.log(`R$ ${price}`)
+})
